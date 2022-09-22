44a34996ae20cbac574d36003f68459e32ca6ad4
Huseyn2003Abdullayev <abdullayevhuseyn05@gmail.com>
2022-09-22 15:33:54 +0400

    Change program.cs

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,2 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello world");
+using Cars;
+
+CarsDataManager carsDataManager = new CarsDataManager();
+Console.WriteLine("Please enter your name.");
+string name = Console.ReadLine();
+Top:
+Console.WriteLine("Hello " + name + ".");
+Console.WriteLine("1.Insert car.");
+Console.WriteLine("2.Update car.");
+Console.WriteLine("3.Delete car.");
+Console.WriteLine("4.Display all cars.");
+int OperationNumber = 0;
+try
+{
+    OperationNumber = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Please insert number.");
+}
+Console.Clear();
+switch (OperationNumber)
+{
+    case 1:
+        carsDataManager.InsertCar();
+        break;
+    case 2:
+        carsDataManager.UpdateCar();
+        break;
+    case 3:
+        carsDataManager.DeleteCar();
+        break;
+    case 4:
+        carsDataManager.DisplayAllCars();
+        break;
+}
+Console.WriteLine("What do you want? ");
+Console.WriteLine("1.Continue");
+Console.WriteLine("2.Leave");
+Answer:
+int Answer = 0;
+try
+{
+    Answer = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Please insert number.");
+    goto Answer;
+}
+Console.Clear();
+switch (Convert.ToInt32(Answer))
+{
+    case 1:
+        goto Top;
+    case 2:
+        goto End;
+    default:
+        Console.WriteLine(" Please type correct number.");
+        goto Answer;
+}
+End:
+Console.ReadKey();
